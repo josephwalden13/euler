7ab9b026b25966f09cf9168fb52628912ce0a37f
Joseph Walden <josephwalden13@gmail.com>
2018-06-11 17:19:28 +0100

    longest collatz sequence

--- problem14.cpp
+++ problem14.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
-int main()
+int cache[(int)1e6];
+bool use_cache = true;
+int collatz(long n, long c = 1)
 {
-    int n = 1004507;
     while(n != 1)
     {
-        cout << n << endl;
+        if (n < 1e6 && use_cache)
+        {
+            if (cache[n] != 0)
+            {
+                c += cache[n];
+                break;
+            }
+        }
+        c++;
         if (n&0x1)
         {
             n = (n << 1) + n + 1;
@@ -15,5 +24,21 @@ int main()
             n = n >> 1;
         }
     }
-    cout << 1 << endl;
+    cache[n] = c;
+    return c;
+}
+int main()
+{
+    int max[2] = {0, 0};
+    for(int i = 1; i != 1e6; i++)
+    {
+        int c = collatz(i);
+        
+        if (c > max[1])
+        {
+            max[1] = c;
+            max[0] = i;
+        }
+    }
+    cout << max[0] << endl;
 }
