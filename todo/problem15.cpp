9dd6d986de24cea1c0bcc844ab2d7e8c76f2356a
Joseph Walden <josephwalden13@gmail.com>
2018-06-11 18:49:48 +0100

    added problem16 to todo list

---
+++ todo/problem15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int paths = 0;
+#define x_max 2
+#define y_max 2
+int pathfinder(int x, int y)
+{
+    
+}
+int main()
+{
+    pathfinder(0,0);
+    cout << paths << endl;   
+}
