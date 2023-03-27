aaf0f429d1ee7e61525bddf016e0089a1a9d49b9
Joseph Walden <josephwalden13@gmail.com>
2023-03-27 23:12:27 +0100

    fixed problem004.cpp

--- problem004.cpp
+++ problem004.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
+#include <string>
 #include <cstdlib>
 
 using namespace std;
@@ -9,18 +9,14 @@ int main() {
     int largest = 0;
     for (int i = 900; i != 999; i++) {
         for (int j = 900; j != 999; j++) {
-            char buffer[7];
-            sprintf(buffer, "%d", (i * j));
-            char reverse[7];
-            for (int i = 5; i >= 0; i--) {
-                reverse[5 - i] = buffer[i];
-            }
-            if (strcmp(reverse, buffer) == 0) {
-                if (atoi(buffer) > largest) {
-                    largest = atoi(buffer);
-                }
+            int number = i * j;
+            string s = to_string(number);
+            string rev = to_string(number);
+            std::reverse(rev.begin(), rev.end());
+            if(s == rev && number > largest) {
+                largest = number;
             }
         }
     }
-    cout << largest << endl;
+    cout << "Largest: " << largest << endl;
 }
